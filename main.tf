600d4af6d1af4d3f263cded4c27c916bb243a80a
Gurpreet <gurp28@my.yorku.ca>
2025-06-21 01:20:33 -0400

    modify

--- main.tf
+++ main.tf
@@ -16,7 +16,9 @@ provider "aws" {
 # Load the code inside the Compute folder
 # This module sets up the compute resources, such as EC2 instances for Kubernetes nodes
 module "Compute" {
-  source            = "./terraform-aws/Compute"
+  source            = "./terraform-aws/Compute" # Path to the Compute module
+ 
+ # Pass the necessary variables to the Compute module
   public_subnet_one = module.Network.public_subnet_one_id   # Pass the public subnet ID from the Network module
   public_subnet_two = [module.Network.public_subnet_two_id] # Pass the public subnet IDs for worker nodes from the Network module
   security_group    = module.Network.security_group_id      # Pass the security group ID from the Network module   
