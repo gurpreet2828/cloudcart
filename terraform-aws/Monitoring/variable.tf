0ad329172a45233f56862ed04462392b22ba1bb9
Gurpreet <gurp28@my.yorku.ca>
2025-06-24 23:41:00 -0400

    region,key,master_dependency added

---
+++ terraform-aws/Monitoring/variable.tf
@@ -0,0 +1,31 @@
+
+variable "aws_region" {
+  description = "The AWS region where the resources will be created."
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "ssh_key_private" {
+  description = "Path to the private SSH key file for accessing the instances."
+  type        = string
+  default     = "~/.ssh/docker"
+}
+
+variable "k8s_master_dependency" {
+  description = "Dependency for the Kubernetes master node to ensure it is created before installing monitoring tools."
+  type        = any
+  default     = null
+}
+
+variable "k8s_master_eip" {
+  description = "Elastic IP address of the Kubernetes master node."
+  type        = string
+  default     = ""
+
+}
+
+variable "fetch_join_command_dependency" {
+  description = "Dependency for fetching the join command to ensure it is available before installing monitoring tools."
+  type        = any
+  default     = null
+}
