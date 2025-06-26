ddbd8d2cd16d7ea1f942a4957725e9352d5099cf
Gurpreet <gurp28@my.yorku.ca>
2025-06-25 22:36:31 -0400

    grafana install, without pvc and nodeport expose

---
+++ terraform-aws/scripts/install_grafana.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ex
+export KUBECONFIG=/etc/kubernetes/admin.conf
+echo 'Installing Helm on the master node...'
+
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo list
+helm repo update
+# Check if the 'monitoring' namespace exists, if not, create it
+kubectl get namespace monitoring >/dev/null 2>&1 || kubectl create namespace monitoring 
+helm search repo grafana/grafana
+# Install grafana using Helm
+helm install my-grafana grafana/grafana --namespace monitoring
+echo "Grafana installed successfully in 'monitoring' namespace."
