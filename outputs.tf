cb17f79b7ae926760f932a2a3e5f7bed1e9e1f42
Gurpreet <gurp28@my.yorku.ca>
2025-06-27 18:18:24 -0400

    grafana and Prometheus URL added

--- outputs.tf
+++ outputs.tf
@@ -14,3 +14,14 @@ output "k8s_worker_connection_info" {
   # This module should return a list of maps, each with keys: public_ip, private_ip, dns_name, ssh_command{    
 }
 
+
+output "prometheus_nodeport_url" {
+  description ="Prometheus UI exposed via NodePort on the master node"
+  value = module.Monitoring.prometheus_nodeport_url
+  
+}
+
+output "grafana_nodeport_url" {
+  description = "Grafana UI exposed via NodePort on the master node"
+  value = module.Monitoring.grafana_nodeport_url
+}
