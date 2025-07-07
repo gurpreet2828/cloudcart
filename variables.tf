a47c97710948a6a5d2896ceaf888b7ffd499bdc3
Gurpreet <gurpreet281988@gmail.com>
2025-07-07 17:39:54 -0400

    eip and k8 master dependency added

--- variables.tf
+++ variables.tf
@@ -4,3 +4,21 @@ variable "aws_region" {
   default     = "us-east-1" # Default region, can be overridden
 
 }
+
+variable "ssh_key_private" {
+  description = "Path to the private SSH key for accessing the instances"
+  type        = string
+  default     = "~/.ssh/docker" # Default path, can be overridden
+  
+}
+variable "k8s_master_dependency" {
+  description = "Dependency for the Kubernetes master node to ensure it is created before installing monitoring tools"
+  type        = any
+  default     = null
+}
+variable "k8s_master_eip" {
+  description = "Elastic IP address of the Kubernetes master node"
+  type        = string
+  default     = null # Default value, can be overridden
+}
+
