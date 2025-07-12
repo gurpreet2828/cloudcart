8ee545623c9c568f0144bda394fbf2eacb630f8a
Gurpreet <gurpreet281988@gmail.com>
2025-07-12 17:45:42 -0400

    ssh private

--- outputs.tf
+++ outputs.tf
@@ -11,6 +11,7 @@ output "k8s_master_connection_info" {
 output "k8s_worker_connection_info" {
   description = "Connection information for the Kubernetes worker nodes"
   value       = module.Compute.k8s_worker_connection_info
+
   # This module should return a list of maps, each with keys: public_ip, private_ip, dns_name, ssh_command{    
 }
 
