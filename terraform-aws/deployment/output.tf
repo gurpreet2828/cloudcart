9a10df0f37864d123f962a4d5dcd61a1dbdbf1c3
Gurpreet <gurp28@my.yorku.ca>
2025-07-11 22:12:52 -0400

    format terraform code

--- terraform-aws/deployment/output.tf
+++ terraform-aws/deployment/output.tf
@@ -1,11 +1,11 @@
 output "deployment_app_nodeport_url" {
   description = "Sock-Shop Application UI exopsed via NodePort on the master node"
-  value = "http://${var.k8s_master_eip}:1050"
-  
+  value       = "http://${var.k8s_master_eip}:1050"
+
 }
 
 output "deployment_app" {
   description = "resource for deployment sock-shop application"
-  value = null_resource.deployment_app.id
-  
+  value       = null_resource.deployment_app.id
+
 }
