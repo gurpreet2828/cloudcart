697d76ec4be7e898c634e5f6d36f91a3ac1a682a
Gurpreet <gurp28@my.yorku.ca>
2025-07-12 18:37:11 -0400

    add comments

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+# this is the main Terraform configuration file for setting up a Kubernetes cluster on AWS.
+# It includes modules for compute resources, network configuration, storage, monitoring, and application deployment.
+
 terraform {
   required_providers {
     aws = {
