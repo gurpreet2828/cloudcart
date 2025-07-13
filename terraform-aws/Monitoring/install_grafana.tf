b7e67c4325d1e1cbf38c327f9623f29d0c4d6a54
Gurpreet <gurp28@my.yorku.ca>
2025-07-13 00:31:22 -0400

    path module

--- terraform-aws/Monitoring/install_grafana.tf
+++ terraform-aws/Monitoring/install_grafana.tf
@@ -67,8 +67,8 @@ resource "null_resource" "install_grafana" {
   #Copy the yaml files from local machine to master EC2 instance
   provisioner "local-exec" {
     command = <<EOT
-   echo "copy entire prometheus yaml files from loacl to master node"
-   scp -o StrictHostKeyChecking=no -i ${var.ssh_key_private} -r /home/administrator/cloudcart/deploy-sock-shop/monitoring-app/grafana ubuntu@${var.k8s_master_eip}:/home/ubuntu/cloudcart/deploy-sock-shop/Monitoring
+   echo "copy entire prometheus yaml files from local to master node"
+   scp -o StrictHostKeyChecking=no -i ${var.ssh_key_private} -r ${path.module}/../../deploy-sock-shop/monitoring-app/grafana ubuntu@${var.k8s_master_eip}:/home/ubuntu/cloudcart/deploy-sock-shop/Monitoring
    EOT
 
   }
