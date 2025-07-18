009aebf8f947a490a19b005c9187408a955e7b05
Gurpreet <gurpreet281988@gmail.com>
2025-07-18 18:35:19 -0400

    terraform provider version upgarded to 6.0 or later

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws" # Get the provider from HashiCorp's registry
-      version = "~> 5.0"        # Use version 5.0 or any newer version within 5.x (not 6.x)
+      version = "~> 6.0"        # Use version 6.0 or any newer version within 6.x (not 7.x)
     }
   }
   required_version = ">= 1.0" # Require Terraform CLI version 1.0 or newer to run this configuration
