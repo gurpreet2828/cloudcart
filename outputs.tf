6441ef63798bcc0e9bef3e491348c983d1aad90e
Gurpreet <gurpreet281988@gmail.com>
2025-07-25 18:24:07 -0400

    output arranged according to module

--- outputs.tf
+++ outputs.tf
@@ -15,18 +15,27 @@ output "k8s_worker_connection_info" {
   # This module should return a list of maps, each with keys: public_ip, private_ip, dns_name, ssh_command{    
 }
 
+output "monitoring_publicip_connection_info" {
+  description = "Connection information for the monitoring tools"
+  value = {
+    prometheus_url = module.Monitoring.prometheus_nodeport_url
+    grafana_url    = module.Monitoring.grafana_nodeport_url
+  }
+ # This module should return a map with keys: prometheus_url, grafana_url, ssh_command{
+}
 
 # output of Prometheus UI exposed via NodePort on the master
-output "prometheus_nodeport_url" {
-  description = "Prometheus UI exposed via NodePort on the master node"
-  value       = module.Monitoring.prometheus_nodeport_url
+# output "prometheus_nodeport_url" {
+#   description = "Prometheus UI exposed via NodePort on the master node"
+#   value       = var.enable_monitoring ? module.Monitoring[0].prometheus_nodeport_url : null
 
-}
+# }
 
-output "grafana_nodeport_url" {
-  description = "Grafana UI exposed via NodePort on the master node"
-  value       = module.Monitoring.grafana_nodeport_url
-}
+# output "grafana_nodeport_url" {
+#   description = "Grafana UI exposed via NodePort on the master node"
+#   value       = module.Monitoring.grafana_nodeport_url
+# }
+
 
 output "deployment_app_nodeport_url" {
   description = "Sock-Shop Application UI exopsed via NodePort on the master node"
@@ -36,4 +45,13 @@ output "deployment_app_nodeport_url" {
 
 output "k8s_alb_dns_name" {
   value = module.Compute.k8s_alb_dns_name
+}
+
+output "Storage_module_status" {
+  description = "value indicating Storage module is enabled or disabled"
+  value = {
+    s3_storage_status     = module.Storage.s3_storage_status
+    dynamodb_table_status = module.Storage.dynamodb_table_status
+    pvc_localstorage_status = module.Storage.pvc_localstorage_status
+  }
 }
