3c2fd088f22534aa3ae9f5b41bf0d5d6d7f863ea
Gurpreet <gurpreet281988@gmail.com>
2025-07-24 20:49:35 -0400

    monitoring_dependency added

--- terraform-aws/Storage/variable.tf
+++ terraform-aws/Storage/variable.tf
@@ -1,3 +1,4 @@
+
 variable "aws_region" {
   description = "AWS region where the resources will be created"
   type        = string
@@ -10,4 +11,36 @@ variable "environment" {
   description = "Environment for tagging resources (e.g., dev, staging, prod)"
   type        = string
   default     = "dev" # Change this to your desired environment
+}
+
+variable "ssh_key_private" {
+  description = "Path to the private SSH key for accessing the instances"
+  type        = string
+  default     = "/home/administrator/.ssh/docker" # Change this to your private key path
+}
+
+# This variable is used to create PVCs for local storage in Kubernetes
+variable "k8s_master_eip" {
+  description = "Elastic IP address of the Kubernetes master node"
+  type        = string
+  default     = ""
+}
+
+variable "k8s_master_dependency" {
+  description = "Dependency for the Kubernetes master node"
+  type        = string
+  default     = ""
+}
+
+variable "fetch_join_command_dependency" {
+  description = "Dependency for fetching the join command"
+  type        = string
+  default     = ""
+}
+
+
+# variable used for PVC Storage
+variable "monitoring_dependency" {
+  description = "Dependency for the monitoring module"
+  type        = any
 }
