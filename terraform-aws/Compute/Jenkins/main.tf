331694e16fd1e0cfe5dc6b45c8df9a9dc81d82d5
Gurpreet <gurpreet281988@gmail.com>
2025-07-27 11:34:55 -0400

    add depends on

--- terraform-aws/Compute/Jenkins/main.tf
+++ terraform-aws/Compute/Jenkins/main.tf
@@ -4,21 +4,26 @@ provider "aws" {
 
 
 resource "aws_key_pair" "aws_jenkins_key" {
-key_name   = "jenkins_key"
-public_key = file("${path.root}/terraform-aws/keys/jenkins_key.pub")
+  key_name   = "jenkins_key"
+  public_key = file("${path.root}/terraform-aws/keys/jenkins_key.pub")
 
-tags = {
-  Name = "Jenkins_Key_Pair"
-}
+  tags = {
+    Name = "Jenkins_Key_Pair"
+  }
 }
 
 
 resource "aws_instance" "jenkins_instance" {
-  ami           = var.jenkins_ami
-  instance_type = var.jenkins_instance_type
-  key_name      = aws_key_pair.aws_jenkins_key.key_name
-  subnet_id     = var.jenkins_public_subnet
-  vpc_security_group_ids = [var.jenkins_sg]
+  depends_on                  = [
+    aws_internet_gateway.jenkins_igw,
+    var.jenkins_public_subnet,
+    var.jenkins_sg
+    ]
+  ami                         = var.jenkins_ami
+  instance_type               = var.jenkins_instance_type
+  key_name                    = aws_key_pair.aws_jenkins_key.key_name
+  subnet_id                   = var.jenkins_public_subnet
+  vpc_security_group_ids      = [var.jenkins_sg]
   associate_public_ip_address = true
 
   user_data = file("terraform-aws/scripts/install_jenkins.sh")
@@ -26,14 +31,14 @@ resource "aws_instance" "jenkins_instance" {
     Name = "Jenkins_Instance"
   }
 
-root_block_device {
-  volume_size = var.jenkins_disk_size
-  volume_type = "gp3"
-  delete_on_termination = true
-  tags = {
-    Name = "Jenkins_Instance_Root_volume"
+  root_block_device {
+    volume_size           = var.jenkins_disk_size
+    volume_type           = "gp3"
+    delete_on_termination = true
+    tags = {
+      Name = "Jenkins_Instance_Root_volume"
+    }
   }
-}
 }
 resource "aws_eip" "jenkins_eip" {
   instance = aws_instance.jenkins_instance.id
@@ -45,21 +50,21 @@ resource "aws_eip" "jenkins_eip" {
 
 resource "null_resource" "jenkins_instance_ready" {
   depends_on = [aws_instance.jenkins_instance, aws_eip.jenkins_eip]
-  
-connection {
-  type        = "ssh"
-  user        = "ubuntu"
-  private_key = file(var.jenkins_key_private)
-  host        = aws_eip.jenkins_eip.public_ip
-}
 
-provisioner "remote-exec" {
-  inline = [
-    "echo 'Jenkins instance is ready!'",
-    "java -version",
-    "jenkins --version",
-    "echo 'Jenkins is installed and running!'",
-  
-  ]
-}
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file(var.jenkins_key_private)
+    host        = aws_eip.jenkins_eip.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Jenkins instance is ready!'",
+      "java -version",
+      "jenkins --version",
+      "echo 'Jenkins is installed and running!'",
+
+    ]
+  }
 }
