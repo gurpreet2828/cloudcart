beaf0ec72e5611f6067c3c404b405a0330713c45
Gurpreet <gurpreet281988@gmail.com>
2025-08-05 21:56:29 -0400

    deleted unused variables

--- variables.tf
+++ variables.tf
@@ -11,17 +11,6 @@ variable "ssh_key_private" {
   default     = "~/.ssh/docker" # Default path, can be overridden
 
 }
-variable "k8s_master_dependency" {
-  description = "Dependency for the Kubernetes master node to ensure it is created before installing monitoring tools"
-  type        = any
-  default     = null
-}
-variable "k8s_master_eip" {
-  description = "Elastic IP address of the Kubernetes master node"
-  type        = string
-  default     = null # Default value, can be overridden
-}
-
 
 variable "jenkins_key_private" {
   description = "Path to the private SSH key file for accessing the Jenkins instance"
@@ -36,4 +25,5 @@ variable "enable_jenkins" {
   type        = bool
   default     = false 
 }
+
 
